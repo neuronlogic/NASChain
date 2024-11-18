31533eca74a6a2bc599aac526e32c92eafcf8ef3
Mutex Locker <mutex@naschain.ai>
2024-11-18 15:31:55 +0000

    cleanup old notebooks

--- run_miner.sh
+++ run_miner.sh
@@ -230,7 +230,7 @@ if [ "$?" -eq 1 ]; then
         if [ -d "./.git" ]; then
 
             # check value on github remotely
-            latest_version=$(check_variable_value_on_github "nimaaghli/NASChain" "src/__init__.py" "__version__ ")
+            latest_version=$(check_variable_value_on_github "neuronlogic/NASChain" "src/__init__.py" "__version__ ")
 
             # If the file has been updated
             if version_less_than $current_version $latest_version; then
